d29eceaacf2c858f114cbdc8f627e9064f2d2c7f
CrimsonTome <crimsontome427@protonmail.com>
2025-01-09 03:35:13 +0000

    Finally, multiple fronter support There is still some work to be done to clean up the code. There is definitely room for improvement.

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ with secretsPath.open(encoding="utf-8") as file:
     secrets: dict[str, str] = json.load(file)
 
 
-def get_data() -> tuple[str, str]:
+def get_data() -> tuple[list[tuple[str, str]], str]:
     """This function gets the fronter data from Simply Plural. \n
     Assume everything here breaks if there are multiple fronters"""
     payload: dict = {}
@@ -30,27 +30,41 @@ def get_data() -> tuple[str, str]:
     data = json.loads(response_content)
     # Get current fronters, this probably breaks if there are multiple fronters
     # Stripping out anything that isn't a member ID such as [] and ''
-    fronters = "".join([item["content"]["member"] for item in data])
+    #fronters = " ".join([item["content"]["member"] for item in data])
+    # ^ not anymore, it's now an array of member IDs
 
+    # get the fronter ids in an array
+    fronter_list = [item["content"]["member"] for item in data]
+    # print(fronter_list)
+    
     # Get uid (system ID)
-    uid = "".join([item["content"]["uid"] for item in data])
+    # only need to get for first member as it's the same for all members
+    uid = data[0]["content"]["uid"]
+    #print(uid)
 
-    # Get the name of the fronter using the uid and member id
-    # This 100% breaks if there are multiple fronters
-    response = requests.request(
-        "GET",
-        SIMPLY_PLURAL_BASE_URL + "/member/" + uid + "/" + fronters,
-        headers=headers,
-        data=payload,
-        timeout=10,
-    )
-    response_content: str = response.text
-    data = json.loads(response_content)
-    name: str = "".join(data["content"]["name"])
+    fronters_concat = []
 
-    avatar_url: str = "".join(data["content"]["avatarUrl"])
+    #suppress warning for now...
+    # pylint: disable=consider-using-enumerate
+    for i in range(len(fronter_list)):
+        response = requests.request(
+            "GET",
+            SIMPLY_PLURAL_BASE_URL + "/member/" + uid + "/" + fronter_list[i],
+            headers=headers,
+            data=payload,
+            timeout=10,
+        )
+        print(response)
+        response_content: str = response.text
+        data = json.loads(response_content)
+        name: str = data["content"]["name"]
+        avatar_url: str = data["content"]["avatarUrl"]
+        fronters_concat.append((name, avatar_url))
 
-    return name, avatar_url
+    # Get the avatar URL of the fronter
+    avatar_url: str = fronters_concat[0][1]
+
+    return fronters_concat, avatar_url
 
 
 # Initialize the client class.
@@ -61,15 +75,20 @@ RPC.connect()
 start_time: float = time.time()
 
 while True:  # The presence will stay on as long as the program is running
-    fronter, avatarURL = get_data()
+    fronters, avatarURL = get_data()
+    # Convert fronters list to a string
+    # what does the underscore do?
+    fronter_details = " & ".join([name for name, _ in fronters])
     # Set the presence and output to console
     if avatarURL:
         print(
             RPC.update(
                 state="(from Simply Plural)",
-                details=fronter,
+                # set details to all active fronter names
+                details=fronter_details,
                 large_image=avatarURL,
-                large_text=fronter,
+                # set large text to first fronter name because the avatar is the first fronter too
+                large_text=fronters[0][0],
                 start=int(start_time),
             )
         )
@@ -77,7 +96,7 @@ while True:  # The presence will stay on as long as the program is running
         print(
             RPC.update(
                 state="(from Simply Plural)",
-                details=fronter,
+                details=fronter_details,
                 start=int(start_time),
             )
         )
